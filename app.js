ce7d746f1ee3d05d3a73e2d5ba81b72ded7b3d16
son1473 <zl6575@naver.com>
2020-06-18 15:50:03 +0900

    Add ctx and startPainting

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext('2d');
+
+ctx.linewidth = 2.5;
+ctx.strokeStyle = "#2c2c2c";	
 
 let painting = false;
+
+function startPainting(){
+	painting = true;
+}
 
 function stopPainting(){
 	painting = false;
@@ -12,6 +20,13 @@ client 좌표는 전체 스크린에서 좌표를 나타낸다.*/
 function onMouseMove(event){
 	const x = event.offsetX;
 	const y = event.offsetY;
+	if(!painting){
+		ctx.beginPath();
+		ctx.moveTo(x, y);
+	} else {
+		ctx.lineTo(x, y);
+		ctx.stroke();
+	}
 }
 
 
@@ -20,17 +35,9 @@ function onMouseDown(event){
 	/*클릭시 그리기 시작*/
 }
 
-function onMouseUp(event){
-	painting = false;
-	// 마우스 클릭을 때면 그리기 중지
-	// 나중을 위해 stopPainting 사용x
-}
-
 if(canvas){
 	canvas.addEventListener("mousemove", onMouseMove);
 	canvas.addEventListener("mousedown", onMouseDown);
-	canvas.addEventListener("mouseup", onMouseUp);
+	canvas.addEventListener("mouseup", stopPainting);
 	canvas.addEventListener("mouseleave", stopPainting); // 마우스가 캔버스를 벗어나기 시작하면 그리기 중지
-	
-
 }
